4eff6835be4fa4a843e1c1d0d0d511b8da284eb4
grilletjesus <grilletjesus@gmail.com>
2018-05-11 00:14:20 +0200

    Blog -> Model created class post blog added to admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    image = models.ImageField(upload_to='post-images/')
+    title = models.CharField(max_length=200)
+    summary = models.TextField(max_length=400)
+    content = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
