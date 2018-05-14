7ea6fce5a5a3eedd39956e1df1d24d7627f4c57a
grilletjesus <grilletjesus@gmail.com>
2018-05-14 11:01:17 +0200

    Contact page added Links of navbar added Allblogs and Detail view Static files

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,4 +6,5 @@ app_name = 'jobs'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -5,3 +5,7 @@ from .models import Job
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs': jobs})
+
+
+def contact(request):
+    return render(request, 'jobs/contact.html')
